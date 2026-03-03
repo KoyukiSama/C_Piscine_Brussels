0b3ff747ee7910edfeb11b4a290d70bf7656f907
Katarina Claes <kaclaes@fu-r7-p6.s19.be>
2026-03-03 17:13:39 +0100

    finish ex01/ft_print_alphabet.c

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 16:54:32 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/03/03 17:09:20 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/03/03 17:12:57 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,18 @@
 void	ft_print_alphabet(void);
 void	ft_putchar(char c);
 
+// int	main(void)
+// {
+// 	ft_print_alphabet();
+// 	return (0);
+// }
+
 void	ft_print_alphabet(void)
 {
 	char	c;
 
 	c = 'a';
-	while(c <= 'z')
+	while (c <= 'z')
 	{
 		ft_putchar(c);
 		c++;
