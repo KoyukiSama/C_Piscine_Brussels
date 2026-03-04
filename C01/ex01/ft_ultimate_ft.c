75c4411ead459aee786c6d81f251b5b8cb06f45c
Katarina Claes <kaclaes@fu-r5-p7.s19.be>
2026-03-04 12:36:42 +0100

    finish ex01/ft_ultimate_ft.c

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/04 12:05:06 by kaclaes           #+#    #+#             */
+/*   Updated: 2026/03/04 12:36:25 by kaclaes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+// #include <unistd.h>
+
+void	ft_ultimate_ft(int *********nbr);
+
+// int main(void)
+// {
+// 	int	nbr = 32;
+// 	int	*nbr_ptr1 = &nbr;
+// 	int	**nbr_ptr2 = &nbr_ptr1;
+// 	int	***nbr_ptr3 = &nbr_ptr2;
+// 	int	****nbr_ptr4 = &nbr_ptr3;
+// 	int	*****nbr_ptr5 = &nbr_ptr4;
+// 	int	******nbr_ptr6 = &nbr_ptr5;
+// 	int	*******nbr_ptr7 = &nbr_ptr6;
+// 	int	********nbr_ptr8 = &nbr_ptr7;
+// 	int	*********nbr_ptr9 = &nbr_ptr8;
+//
+// 	ft_ultimate_ft(nbr_ptr9);
+// 	printf("%i\n", nbr);
+// 	write(1, nbr, 1);
+// }
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
