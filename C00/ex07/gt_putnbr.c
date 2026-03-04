6227bc9949f6b1104b540f1eb556d84be1d8d2d1
Katarina Claes <kaclaes@fu-r5-p7.s19.be>
2026-03-04 16:23:23 +0100

    ex07

--- C00/ex07/gt_putnbr.c
+++ C00/ex07/gt_putnbr.c
@@ -6,18 +6,19 @@
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 13:23:14 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/03/04 14:30:14 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/03/04 16:22:24 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 void	ft_putnbr(int nbr);
 void	ft_putchar(char c);
 
 int	main(void)
 {
-	ft_putnbr(-2147483648);
+	ft_putnbr(1489184);
 }
 
 void	ft_putnbr(int nbr)
@@ -26,21 +27,22 @@ void	ft_putnbr(int nbr)
 	long	len;
 	char	str[10];
 
-	if (nbr == 0)
-		return (ft_putchar('0'));
-	if (nbr < 0)
-		return (ft_putchar('-'));
 	lng = nbr;
 	len = 0;
+	if (nbr == 0)
+		return (ft_putchar('0'));
+	if (lng < 0)
+	{
+		ft_putchar('-');
+		lng *= -1;
+	}
 	while (lng)
 	{
-		str[len] = (lng % 10) + '0';
-		ft_putchar(str[len]);
+		str[len++] = (lng % 10) + '0';
 		lng /= 10;
-		len++;
 	}
 	while (len)
-		ft_putchar(*(str + len--));
+		ft_putchar(str[--len]);
 }
 
 void	ft_putchar(char c)
