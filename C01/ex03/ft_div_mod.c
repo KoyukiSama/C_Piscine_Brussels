02427e777f19995a402ada4f5cd9e9030113a248
Katarina Claes <kaclaes@fu-r5-p7.s19.be>
2026-03-04 17:38:04 +0100

    finish ex02 ex03

--- C01/ex03/ft_div_mod.c
+++ C01/ex03/ft_div_mod.c
@@ -1,33 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/04 17:20:07 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/03/04 17:26:54 by kaclaes          ###   ########.fr       */
+/*   Created: 2026/03/04 17:26:42 by kaclaes           #+#    #+#             */
+/*   Updated: 2026/03/04 17:37:48 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include <stdio.h>
 
-void	ft_swap(int *a, int *b);
+void	ft_div_mod(int a, int b, int *div, int *mod);
 
 // int	main(void)
 // {
-// 	int	a = 4;
-// 	int	b = 2;
-// 	printf("%i, %i\n", a, b);
-// 	ft_swap(&a, &b);
-// 	printf("%i, %i\n", a, b);
+// 	int	a = 45;
+// 	int	b = 10;
+// 	int	div = 0;
+// 	int mod = 0;
+
+// 	ft_div_mod(a, b, &div, &mod);
+// 	printf("%i, %i, %i, %i\n", a, b, div, mod);
 // }
 
-void	ft_swap(int *a, int *b)
+void	ft_div_mod(int a, int b, int *div, int *mod)
 {
-	int	swap;
-
-	swap = *a;
-	*a = *b;
-	*b = swap;
+	*div = a / b;
+	*mod = a % b;
 }
