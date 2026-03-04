ee6b2ff7fbb66c7280ef1470cb538fd242e8f3b3
Katarina Claes <kaclaes@fu-r5-p7.s19.be>
2026-03-04 13:31:26 +0100

    finish ex06/ft_print_comb2.c

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/04 13:23:14 by kaclaes           #+#    #+#             */
+/*   Updated: 2026/03/04 13:28:05 by kaclaes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_comb(void);
+int		ft_putdig(char dig1, char dig2, char dig3);
+void	ft_putchar(char c);
+
+// int	main(void)
+// {
+// 	ft_print_comb();
+// }
+
+void	ft_print_comb(void)
+{
+	char	dig1;
+	char	dig2;
+	char	dig3;
+
+	dig1 = '0';
+	dig2 = '1';
+	dig3 = '2';
+	while (dig1 <= '7')
+	{
+		while (dig2 <= '8')
+		{
+			while (dig3 <= '9')
+			{
+				if (ft_putdig(dig1, dig2, dig3))
+					return ;
+				dig3++;
+			}
+			//ft_putchar('\n');
+			dig2++;
+			dig3 = dig2 + 1;
+		}
+		dig1++;
+		dig2 = dig1 + 1;
+		dig3 = dig2 + 1;
+	}
+}
+
+int	ft_putdig(char dig1, char dig2, char dig3)
+{
+	ft_putchar(dig1);
+	ft_putchar(dig2);
+	ft_putchar(dig3);
+	if (dig1 == '7' && dig2 == '8' && dig3 == '9')
+		return (1);
+	return (ft_putchar(','), ft_putchar(' '), 0);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
