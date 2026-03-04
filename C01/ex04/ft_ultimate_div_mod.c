81995f05343b910e92a2bf591af2830bdb9bdb41
Katarina Claes <kaclaes@fu-r5-p7.s19.be>
2026-03-04 17:42:06 +0100

    finish ex03 ex04

--- C01/ex04/ft_ultimate_div_mod.c
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -1,32 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 17:26:42 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/03/04 17:38:40 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/03/04 17:41:49 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+// #include <stdio.h>
 
-void	ft_div_mod(int a, int b, int *div, int *mod);
+void	ft_ultimate_div_mod(int *a, int *b);
 
 // int	main(void)
 // {
 // 	int	a = 45;
 // 	int	b = 10;
-// 	int	div = 0;
-// 	int mod = 0;
 
-// 	ft_div_mod(a, b, &div, &mod);
-// 	printf("%i, %i, %i, %i\n", a, b, div, mod);
+// 	ft_ultimate_div_mod(&a, &b);
+// 	printf("%i, %i\n", a, b);
 // }
 
-void	ft_div_mod(int a, int b, int *div, int *mod)
+void	ft_ultimate_div_mod(int *a, int *b)
 {
-	*div = a / b;
-	*mod = a % b;
+	int	tmp_a;
+	int	tmp_b;
+
+	tmp_a = *a;
+	tmp_b = *b;
+	*a = tmp_a / tmp_b;
+	*b = tmp_a % tmp_b;
 }
+
