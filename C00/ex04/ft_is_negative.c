6badf75136ec1c1bc5738c9556c69eb1eca042f8
Katarina Claes <kaclaes@fu-r5-p7.s19.be>
2026-03-04 12:39:49 +0100

    finish ex04/ft_is_negative.c

--- C00/ex04/ft_is_negative.c
+++ C00/ex04/ft_is_negative.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 22:41:18 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/03/03 22:41:18 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/03/04 12:39:03 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,17 @@
 void	ft_is_negative(int n);
 void	ft_putchar(char c);
 
-int	main(void)
-{
-	ft_is_negative();
-}
+// int	main(void)
+// {
+// 	ft_is_negative(-10);
+// 	ft_is_negative(0);
+// 	ft_is_negative(10);
+// }
 
 void	ft_is_negative(int n)
 {
 	if (n < 0)
-		ft_putchar('N');
+		return (ft_putchar('N'));
 	ft_putchar('P');
 }
 
