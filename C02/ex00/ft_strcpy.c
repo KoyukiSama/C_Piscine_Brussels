b454c5551fe19717c301d148adb1f7433fdd42dd
Katarina Claes <kaclaes@fu-r7-p7.s19.be>
2026-03-05 18:16:36 +0100

    add ex00

---
+++ C02/ex00/ft_strcpy.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/05 18:03:59 by kaclaes           #+#    #+#             */
+/*   Updated: 2026/03/05 18:16:18 by kaclaes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//char	*ft_strcpy(char *dest, char *src);
+
+#include <string.h>
+int	main(void)
+{
+	char dest[10];
+	char src[] = "hello";
+	strcpy(dest, src);
+	//ft_strcpy(dest, src);
+}
+
+// char	*ft_strcpy(char *dest, char *src)
+// {
+	
+// }
