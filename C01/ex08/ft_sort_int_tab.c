01972b5a3637e83f01f294afa8f506f0daa79adf
Katarina Claes <kaclaes@fu-r7-p7.s19.be>
2026-03-05 17:16:13 +0100

    norm ex08

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -6,48 +6,45 @@
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 15:54:13 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/03/05 17:14:18 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/03/05 17:15:56 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_sort_int_tab(int *tab, int size);
+int		ft_swap(int *a, int *b);
 
-#include <stdlib.h>
-#include <stdio.h>
-void	print_tab(int *tab, int size)
-{
-	size_t	i = 0;
-
-	printf("[");
-	while (i < size)
-	{
-		printf("%i", tab[i++]);
-		if (i < size)
-			printf(", ");
-	}
-	printf("]");
-	printf("\n");
-}
-int	main(int argc, char **argv)
-{
-	size_t	i = 0;
-	int	tab_size = argc - 1;
-
-	if (argc < 2)
-		return (1);
-	int *tab = malloc(sizeof(int) * (tab_size));
-	while (i < tab_size)
-	{
-		tab[i] = atoi(argv[i + 1]);
-		i++;
-	}
-	print_tab(tab, tab_size);
-	ft_sort_int_tab(tab, tab_size);
-	print_tab(tab, tab_size);
-	free(tab);
-}
-
-int	ft_swap(int *a, int *b);
+// #include <stdlib.h>
+// #include <stdio.h>
+// void	print_tab(int *tab, int size)
+// {
+// 	size_t	i = 0;
+// 	printf("[");
+// 	while (i < size)
+// 	{
+// 		printf("%i", tab[i++]);
+// 		if (i < size)
+// 			printf(", ");
+// 	}
+// 	printf("]");
+// 	printf("\n");
+// }
+// int	main(int argc, char **argv)
+// {
+// 	size_t	i = 0;
+// 	int	tab_size = argc - 1;
+// 	if (argc < 2)
+// 		return (1);
+// 	int *tab = malloc(sizeof(int) * (tab_size));
+// 	while (i < tab_size)
+// 	{
+// 		tab[i] = atoi(argv[i + 1]);
+// 		i++;
+// 	}
+// 	print_tab(tab, tab_size);
+// 	ft_sort_int_tab(tab, tab_size);
+// 	print_tab(tab, tab_size);
+// 	free(tab);
+// }
 
 void	ft_sort_int_tab(int *tab, int size)
 {
