e9482bc730098bf163e6b4dcfd40ff9a84bc9066
Katarina Claes <kaclaes@fu-r9-p6.s19.be>
2026-03-09 14:13:37 +0100

    fin ex04, ex05

--- C02/ex05/ft_str_is_uppercase.c
+++ C02/ex05/ft_str_is_uppercase.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/09 14:02:51 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/03/09 14:11:33 by kaclaes          ###   ########.fr       */
+/*   Created: 2026/03/09 14:10:53 by kaclaes           #+#    #+#             */
+/*   Updated: 2026/03/09 14:13:07 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_str_is_lowercase(char *str);
+int	ft_str_is_uppercase(char *str);
 
 // #include <stdio.h>
 // int main(int argc, char **argv)
 // {
 // 	if (argc != 2)
 // 		return (1);
-// 	printf("%i", ft_str_is_lowercase(argv[1]));
+// 	printf("%i", ft_str_is_uppercase(argv[1]));
 // }
 
-int	ft_str_is_lowercase(char *str)
+int	ft_str_is_uppercase(char *str)
 {
-	while (*str >= 'a' && *str <= 'z')
+	while (*str >= 'A' && *str <= 'Z')
 		str++;
 	if (*str == '\0')
 		return (1);
