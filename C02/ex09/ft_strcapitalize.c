80b477671a2bc4a3db96de974defd409a8ad053a
Katarina Claes <kaclaes@fu-r9-p6.s19.be>
2026-03-09 15:52:19 +0100

    add ex09

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,38 +6,34 @@
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 14:29:16 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/03/09 14:47:54 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/03/09 15:50:48 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strcapitalize(char *str);
-int	ft_is_alphanum(char c);
-int	ft_is_num(char c);
-int	ft_is_lower(char c);
+char	*ft_strcapitalize(char *str);
+int		ft_is_alphanum(char c);
+int		ft_is_lower(char c);
 
-#include <stdio.h>
-int main(int argc, char **argv)
-{
-	if (argc != 2)
-		return (1);
-	printf("%s\n", ft_strcapitalize(argv[1]));
-}
+// #include <stdio.h>
+// int main(int argc, char **argv)
+// {
+// 	if (argc != 2)
+// 		return (1);
+// 	printf("%s\n", ft_strcapitalize(argv[1]));
+// }
 
-char *ft_strcapitalize(char *str)
+char	*ft_strcapitalize(char *str)
 {
 	int	i;
 
 	i = 0;
 	while (str[i])
 	{
-		if (ft_is_alphanum(str[i]))
-		{
-			if (ft_is_lower(str[i]))
-				str[i] += 'A' - 'a';
-			while (ft_is_alphanum(str[i]))
-				i++;
-		}
-		while (!ft_is_alphanum(str[i]))
+		if (ft_is_lower(str[i]))
+			str[i++] += 'A' - 'a';
+		while (str[i] && ft_is_alphanum(str[i]))
+			i++;
+		while (str[i] && !ft_is_alphanum(str[i]))
 			i++;
 	}
 	return (str);
@@ -45,14 +41,7 @@ char *ft_strcapitalize(char *str)
 
 int	ft_is_lower(char c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
-}
-
-int	ft_is_num(char c)
-{
-	if (c >= '0' && c <= '9')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	return (0);
 }
