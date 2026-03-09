df770e55b75092805ba7ab93b9b90a599ada12c4
Katarina Claes <kaclaes@fu-r9-p6.s19.be>
2026-03-09 14:16:35 +0100

    fin ex06

---
+++ C02/ex06/ft_str_is_printable.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/09 14:14:34 by kaclaes           #+#    #+#             */
+/*   Updated: 2026/03/09 14:15:36 by kaclaes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_printable(char *str);
+
+// #include <stdio.h>
+// int main(int argc, char **argv)
+// {
+// 	if (argc != 2)
+// 		return (1);
+// 	printf("%i", ft_str_is_uppercase(argv[1]));
+// }
+
+int	ft_str_is_printable(char *str)
+{
+	while (*str >= 32 && *str <= 126)
+		str++;
+	if (*str == '\0')
+		return (1);
+	return (0);
+}
