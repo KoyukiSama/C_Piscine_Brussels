4d80c25d349ad7666eacca2b1c64addc2ad07ccc
Katarina Claes <kaclaes@fu-r8-p7.s19.be>
2026-03-10 15:31:12 +0100

    fix ex10

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -6,11 +6,12 @@
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 16:52:39 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/03/09 20:05:46 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/03/10 15:30:50 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
+int				ft_strlen(char *str);
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
@@ -23,5 +24,15 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 		i++;
 	}
 	dest[i] = '\0';
-	return (ft_strlen(dest));
+	return (ft_strlen(src));
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
