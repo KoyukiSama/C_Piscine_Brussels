f37eb0335c31ab90753b1e102448ca17f5d22674
Katarina Claes <kaclaes@fu-r8-p8.s19.be>
2026-03-11 13:08:55 +0100

    fix ex04

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,26 +6,26 @@
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 12:41:59 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/03/11 13:06:25 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/03/11 13:08:38 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 char	*ft_strstr(char *str, char *to_find);
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-int main(int argc, char **argv)
-{
-	char	dest[30];
-	char	ft_dest[30];
-	if (argc != 3)
-		return (1);
-	strcpy(dest, argv[1]);
-	strcpy(ft_dest, argv[1]);
-	printf("   strstr: %s\n", strstr(dest, argv[2]));
-	printf("ft_strstr: %s\n", ft_strstr(ft_dest, argv[2]));
-}
+// #include <stdio.h>
+// #include <string.h>
+// int main(int argc, char **argv)
+// {
+// 	char	dest[30];
+// 	char	ft_dest[30];
+// 	if (argc != 3)
+// 		return (1);
+// 	strcpy(dest, argv[1]);
+// 	strcpy(ft_dest, argv[1]);
+// 	printf("   strstr: %s\n", strstr(dest, argv[2]));
+// 	printf("ft_strstr: %s\n", ft_strstr(ft_dest, argv[2]));
+// }
 
 char	*ft_strstr(char *str, char *to_find)
 {
