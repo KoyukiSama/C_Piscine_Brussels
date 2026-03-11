e5336a5e4c7bc22d4e2d6dcfec04ea59a20c009e
Katarina Claes <kaclaes@fu-r8-p8.s19.be>
2026-03-11 12:11:16 +0100

    add ex02

--- C03/ex02/ft_strcat.c
+++ C03/ex02/ft_strcat.c
@@ -6,26 +6,26 @@
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 14:42:54 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/03/10 14:58:25 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/03/11 12:11:03 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char *ft_strcat(char *dest, char *src);
 
-#include <stdio.h>
-#include <string.h>
-int main(int argc, char **argv)
-{
-	char	dest[30];
-	char	ft_dest[30];
+// #include <stdio.h>
+// #include <string.h>
+// int main(int argc, char **argv)
+// {
+// 	char	dest[30];
+// 	char	ft_dest[30];
 
-	if (argc != 3)
-		return (1);
-	strcat(dest, argv[1]);
-	strcat(ft_dest, argv[1]);
-	printf("   strcat_dest: %s\n", strcat(dest, argv[2]));
-	printf("ft_strcat_dest: %s\n", strcat(ft_dest, argv[2]));
-}
+// 	if (argc != 3)
+// 		return (1);
+// 	strcpy(dest, argv[1]);
+// 	strcpy(ft_dest, argv[1]);
+// 	printf("   strcat_dest: %s\n", strcat(dest, argv[2]));
+// 	printf("ft_strcat_dest: %s\n", ft_strcat(ft_dest, argv[2]));
+// }
 
 char *ft_strcat(char *dest, char *src)
 {
