62d96afad5237a9937b63ca78699bee4d80c793a
Katarina Claes <kaclaes@fu-r8-p8.s19.be>
2026-03-12 13:37:34 +0100

    add ex00

--- C04/ex00/ft_strlen.c
+++ C04/ex00/ft_strlen.c
@@ -6,26 +6,26 @@
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 13:33:12 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/03/12 13:35:54 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/03/12 13:37:23 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strlen(char *str);
 
-#include <stdio.h>
-int	main(int argc, char **argv)
-{
-	if (argc != 2)
-		return (1);
-	printf("len: %i\n", ft_strlen(argv[1]));
-}
+// #include <stdio.h>
+// int	main(int argc, char **argv)
+// {
+// 	if (argc != 2)
+// 		return (1);
+// 	printf("len: %i\n", ft_strlen(argv[1]));
+// }
 
 int	ft_strlen(char *str)
 {
 	int	len;
 
 	len = 0;
-	while(str[len])
+	while (str[len])
 		len++;
 	return (len);
 }
