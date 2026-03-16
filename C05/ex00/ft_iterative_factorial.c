ca4168c10aab716f5ba2dbafe6dea579b64fa406
Katarina Claes <kaclaes@fu-r9-p8.s19.be>
2026-03-16 13:59:04 +0100

    add ex00

---
+++ C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/16 13:31:42 by kaclaes           #+#    #+#             */
+/*   Updated: 2026/03/16 13:41:09 by kaclaes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb);
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// int	main(int argc, char **argv)
+// {
+// 	if (argc != 2)
+// 		return (1);
+// 	printf("%i", ft_iterative_factorial(atoi(argv[1])));
+// }
+
+int	ft_iterative_factorial(int nb)
+{
+	int	res;
+
+	res = 1;
+	while (nb > 1)
+	{
+		res *= nb;
+		nb--;
+	}
+	return (res);
+}
