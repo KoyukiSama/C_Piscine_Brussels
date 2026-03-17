d17c167a553be6be94e928d4b637081c0f5826c7
Katarina Claes <kaclaes@fu-r9-p8.s19.be>
2026-03-17 13:48:21 +0100

    fix norms

--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -1,29 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_range.c                                         :+:      :+:    :+:   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 12:41:19 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/03/17 13:47:52 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/03/17 13:48:05 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int	*ft_range(int min, int max);
+int	ft_ultimate_range(int **range, int min, int max);
 
 // #include <stdio.h>
 // int	main(int argc, char **argv)
 // {
 // 	if (argc != 3)
 // 		return (1);
-// 	int	*arr = ft_range(atoi(argv[1]), atoi(argv[2]));
+// 	int	*arr;
+// 	int	size = ft_ultimate_range(&arr, atoi(argv[1]), atoi(argv[2]));
 // 	if (!arr)
 // 		return (2);
 // 	int	i = 0;
-// 	int	size = atoi(argv[2]) - atoi(argv[1]) + 1;
 // 	printf("[");
 // 	while (i < size)
 // 	{
@@ -35,20 +35,20 @@ int	*ft_range(int min, int max);
 // 	free(arr);
 // }
 
-int	*ft_range(int min, int max)
+int	ft_ultimate_range(int **range, int min, int max)
 {
 	int	size;
 	int	*arr;
 	int	i;
 
 	if (min >= max)
-		return (NULL);
+		return (*range = NULL, 0);
 	size = max - min + 1;
 	arr = malloc(size * sizeof(int));
 	if (!arr)
-		return (NULL);
+		return (-1);
 	i = 0;
 	while (i < size)
 		arr[i++] = min++;
-	return (arr);
+	return (*range = arr, size);
 }
