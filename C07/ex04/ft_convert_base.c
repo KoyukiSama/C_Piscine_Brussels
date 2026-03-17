2c2d97eb3b863cc4f83b4647516cb858151caf3c
Katarina Claes <kaclaes@fu-r9-p8.s19.be>
2026-03-17 15:38:10 +0100

    add ex04

---
+++ C07/ex04/ft_convert_base.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/17 15:27:03 by kaclaes           #+#    #+#             */
+/*   Updated: 2026/03/17 15:38:00 by kaclaes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to);
+
+int	main(int argc, char **argv)
+{
+	
+}
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+
+}
