5f455b075e3865f4005cb4ad358abf0d9fc53bd5
Katarina Claes <kaclaes@fu-r9-p8.s19.be>
2026-03-17 12:39:49 +0100

    fix ex00 norm and add

--- C07/ex00/ft_strdup.c
+++ C07/ex00/ft_strdup.c
@@ -6,28 +6,29 @@
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 12:27:58 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/03/17 12:38:10 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/03/17 12:39:37 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+
 char	*ft_strdup(char *src);
 
-#include <stdio.h>
-#include <string.h>
-int	main(int argc, char **argv)
-{
-	if (argc != 2)
-		return (1);
-	char	*dup1 = strdup(argv[1]);
-	char	*dup2 = ft_strdup(argv[1]);
-	printf("   strdup: %s\n", dup1);
-	printf("ft_strdup: %s\n", dup2);
-	free(dup1);
-	free(dup2);
-}
+// #include <stdio.h>
+// #include <string.h>
+// int	main(int argc, char **argv)
+// {
+// 	if (argc != 2)
+// 		return (1);
+// 	char	*dup1 = strdup(argv[1]);
+// 	char	*dup2 = ft_strdup(argv[1]);
+// 	printf("   strdup: %s\n", dup1);
+// 	printf("ft_strdup: %s\n", dup2);
+// 	free(dup1);
+// 	free(dup2);
+// }
 
-int	ft_strlen(char *str);
+int		ft_strlen(char *str);
 
 char	*ft_strdup(char *src)
 {
@@ -49,8 +50,9 @@ char	*ft_strdup(char *src)
 
 int	ft_strlen(char *str)
 {
-	int	len = 0;
-	
+	int	len;
+
+	len = 0;
 	while (str[len])
 		len++;
 	return (len);
