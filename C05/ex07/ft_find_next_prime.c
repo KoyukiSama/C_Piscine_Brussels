afe59eae627b4c74efa69c1ec87757ab69e1b595
Katarina Claes <kaclaes@fu-r6-p8.s19.be>
2026-03-19 15:19:56 +0100

    add ex07

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -6,21 +6,21 @@
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 14:50:46 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/03/19 15:17:29 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/03/19 15:19:48 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_is_prime(int nb);
 int	ft_find_next_prime(int nb);
 
-#include <stdio.h>
-#include <stdlib.h>
-int	main(int argc, char **argv)
-{
-	if (argc != 2)
-		return (1);
-	printf("%i", ft_find_next_prime(atoi(argv[1])));
-}
+// #include <stdio.h>
+// #include <stdlib.h>
+// int	main(int argc, char **argv)
+// {
+// 	if (argc != 2)
+// 		return (1);
+// 	printf("%i", ft_find_next_prime(atoi(argv[1])));
+// }
 
 int	ft_find_next_prime(int nb)
 {
