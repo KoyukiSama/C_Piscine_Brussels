3b258bba0b044db789cbf893b3307e197c40f68a
Katarina Claes <kaclaes@fu-r8-p7.s19.be>
2026-03-25 16:09:38 +0100

    add ex04

---
+++ C08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/25 15:35:47 by kaclaes           #+#    #+#             */
+/*   Updated: 2026/03/25 16:09:26 by kaclaes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+// #include <stdio.h>
+// typedef struct s_stock_str
+// {
+// 	int		size;
+// 	char	*str;
+// 	char	*copy;
+// }	t_stock_str;
+// struct s_stock_str	*ft_strs_to_tab(int ac, char **av);
+// int	main(int argc, char **argv)
+// {
+// 	t_stock_str *strs = ft_strs_to_tab(argc, argv);
+// 	int			i = 0;
+
+// 	while (i < argc)
+// 	{
+// 		printf("strs[%i] {\nstrs.strs: \"%s\"\n", i, strs[i].str);
+// 		printf("strs.cpy:  \"%s\"\n", strs[i].copy);
+// 		printf("strs.size:  %i\n}\n\n", strs[i].size);
+// 		free(strs[i].copy);
+// 		i++;
+// 	}
+// 	free(strs);
+// 	return (0);
+// }
+
+int		ft_strlen(char *str);
+char	*ft_strdup(char *str);
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	t_stock_str	*str_array;
+	int			i;
+
+	str_array = malloc(sizeof(*str_array) * ac);
+	if (!str_array)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		str_array[i].str = av[i];
+		str_array[i].size = ft_strlen(av[i]);
+		str_array[i].copy = ft_strdup(av[i]);
+		if (str_array[i].copy == NULL)
+		{
+			i--;
+			while (i >= 0)
+				free(str_array[i--].copy);
+			free(str_array);
+			return (NULL);
+		}
+		i++;
+	}
+	return (str_array);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *str)
+{
+	int		i;
+	char	*cpy;
+
+	cpy = malloc(ft_strlen(str) + 1);
+	i = 0;
+	while (str[i])
+	{
+		cpy[i] = str[i];
+		i++;
+	}
+	cpy[i] = '\0';
+	return (cpy);
+}
